6cbfb9e6687bb32d1c1455337ac64f23a18ea052
tuxy <lastpass7565@gmail.com>
2024-11-29 08:15:31 +1000

    Added main system, 404

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,42 @@ use html_escape;
 use tiny_http::{Server, Response};
 
 fn main() {
-    let server = Server::http("127.0.0.1:7878").unwrap();
+    let server = Server::http("127.0.0.1:7878").expect("Could not bind to address.");
 
     for request in server.incoming_requests() {
-        println!("received request! method: {:?}, url: {:?}, headers: {:?}",
-            request.method(),
-            request.url(),
-            request.headers()
-        );
 
-        let content = fs::read_to_string("dist/index.md").unwrap();
+        // Get and parse request.url(), which returns something like /test
+        // Reads from .md file
+        let filename = match request.url() {
+            "/" => "/index",
+            _ => request.url(),
+        };
+
+        // Parses the filename for the server. More realistic and integrated way to do this?
+        let filename = {
+            let mut directory = String::from("dist");
+            directory.push_str(&filename);
+            directory.push_str(".md");
+            directory
+        };
+
+        // If page not found. TODO add reason
+        let content = match fs::read_to_string(filename) {
+            Ok(val) => val,
+            Err(_) => fs::read_to_string("dist/404.md").expect("Error reading files. Is 404.md there?"),
+        };
+
+        // Converts to html
         let html = markdown::to_html(&content);
-        //println!("{html}");
         let mut html_decoded = String::new();
+
+        // Uses a LIBRARY to remove html escapes. PLS change it sucks
+        // unsafe { from_utf8_unchecked(decode_html_entities_to_vec(text, output.as_mut_vec())) }
+        // UP THERE is the unsafe version. We could use it like that, but lets see...
         html_escape::decode_html_entities_to_string(html, &mut html_decoded);
         let response = Response::from_data(html_decoded);
+
+        // Why does this suck
         let _ = request.respond(response);
     }
-
 }
