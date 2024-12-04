e202641cb254886d8d15fbda856a31870c92fd00
tuxy <lastpass7565@gmail.com>
2024-12-04 09:12:26 +0700

    Refactored bar.rs

---
+++ src/bar.rs
@@ -0,0 +1,48 @@
+use tiny_http::Request;
+use std::fs;
+
+use crate::config::Config;
+
+pub fn custom_markdown(request: &Request, config: &Config) -> String {
+    // Get and parse request.url(), which returns something like /test
+        // Reads from .md file
+        let filename = match request.url() {
+            "/" => "/index",
+            _ => request.url(),
+        };
+
+        // Parses the filename for the server. More realistic and integrated way to do this?
+        let filename = {
+            let mut directory = String::from("dist");
+            directory.push_str(&filename);
+            directory.push_str(".md");
+            directory
+        };
+
+        let mut bar_contents = String::new();
+        // Checks if bar table? in the config is populated.
+        match config.bar {
+            Some(ref val) => {
+                let item_names = &val.names;
+                let item_url = &val.urls;
+                for (idx, i) in item_names.iter().enumerate() {
+                    bar_contents.push_str(
+                        format!("/ [{}]({}) ", i, item_url[idx]).as_str()
+                    );
+                }
+            },
+            None => (),
+        }
+        // Acts as both a spacer for the top of the page and to space the navigation bar away from content.
+        bar_contents.push_str("\n");
+
+        // If page not found. TODO add reason
+        let content = match fs::read_to_string(filename) {
+            Ok(val) => val,
+            Err(_) => fs::read_to_string("dist/404.md").expect("Error reading 404.md. Is 404.md there?"),
+        };
+
+        bar_contents.push_str(&content.as_str());
+
+        bar_contents
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::fs;
 use tiny_http::{Server, Response};
 
 mod parse;
 mod config;
+mod bar;
 
 fn main() {
 
@@ -14,47 +14,10 @@ fn main() {
 
     for request in server.incoming_requests() {
 
-        // Get and parse request.url(), which returns something like /test
-        // Reads from .md file
-        let filename = match request.url() {
-            "/" => "/index",
-            _ => request.url(),
-        };
-
-        // Parses the filename for the server. More realistic and integrated way to do this?
-        let filename = {
-            let mut directory = String::from("dist");
-            directory.push_str(&filename);
-            directory.push_str(".md");
-            directory
-        };
-
-        let mut bar_contents = String::new();
-        // Checks if bar table? in the config is populated.
-        match config.bar {
-            Some(ref val) => {
-                let item_names = &val.names;
-                let item_url = &val.urls;
-                for (idx, i) in item_names.iter().enumerate() {
-                    bar_contents.push_str(
-                        format!("/ [{}]({}) ", i, item_url[idx]).as_str()
-                    );
-                }
-            },
-            None => (),
-        }
-        bar_contents.push_str("\n");
-
-        // If page not found. TODO add reason
-        let content = match fs::read_to_string(filename) {
-            Ok(val) => val,
-            Err(_) => fs::read_to_string("dist/404.md").expect("Error reading 404.md. Is 404.md there?"),
-        };
-
-        bar_contents.push_str(&content.as_str());
+        let content= bar::custom_markdown(&request, &config);
         // Converts to html with options
 
-        let html = parse::parse(&bar_contents, request.url(), &config.custom_css);
+        let html = parse::parse(&content, request.url(), &config.custom_css);
 
         let response = Response::from_data(html);
 
