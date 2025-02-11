2217a8ca0b7b12d3756ca85a7e6c67f65c2e6d3f
tuxy <lastpass7565@gmail.com>
2025-02-11 23:43:21 +1000

    Basic static cache implemented

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,49 @@
+use std::{ fs, collections::HashMap };
+
 use axum::{Router, routing::get};
 use tokio::net::TcpListener;
 
 mod parse;
 mod pages;
 
+#[derive(Default, Debug, Clone)]
+struct Cache {
+    cache: HashMap<String, String>
+}
+
+impl Cache {
+
+    // Not very performant but is a usable and simple cache
+    fn cache_pages() -> Cache {
+        let mut state = Cache {
+            cache: HashMap::new()
+        };
+
+        let paths = fs::read_dir("dist").unwrap();
+        for path in paths {
+            let path = path.unwrap().path().clone();
+
+            let file = match fs::read_to_string(path.display().to_string()) {
+                Ok(val) => val,
+                Err(_) => fs::read_to_string("dist/404.md")
+                    .expect("404.md doesn't exist")
+            };
+
+            state.cache.insert(
+                String::from(path.file_name().unwrap().to_str().unwrap()),
+                file
+            );
+        }
+        state
+    }
+}
+
 #[tokio::main]
 async fn main() {
+    let state = Cache::cache_pages();
+
     let app = Router::new()
-        .route("/{path}", get(pages::adaptive_page));
+        .route("/{path}", get(pages::adaptive_page).with_state(state));
 
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
     println!("Listening on {}", listener.local_addr().unwrap());
--- src/pages.rs
+++ src/pages.rs
@@ -1,35 +1,19 @@
 use std::fs;
 
-use axum::{ extract::Path, response::Html };
-use crate::parse::Parser;
+use axum::{ extract::{Path, State}, response::Html };
+use crate::{parse::Parser, Cache};
 
-pub async fn adaptive_page(Path(path): Path<String>) -> Html<String> {
-    let markdown = load_page(&path);
+pub async fn adaptive_page(Path(path): Path<String>, State(state): State<Cache>) -> Html<String> {
+
+    let path = path + ".md";
+
+    let markdown = state.cache.get(&path).unwrap();
+    
     let parser = Parser {
-        content: markdown,
+        content: markdown.to_string(),
         path: path,
         css: String::from("https://cdn.simplecss.org/simple.css")
     };
 
     Html(parser.parse())
-}
-
-fn load_page(path: &str) -> String {
-    // Get and parse request.url(), which returns something like /test
-    // Reads from .md file
-    let filename = format!("/{path}");
-
-    // Parses the filename for the server. More realistic and integrated way to do this?
-    let filename = {
-        let mut directory = String::from("dist");
-        directory.push_str(&filename);
-        directory.push_str(".md");
-        directory
-    };
-
-    // Return 404 if doesn't exist
-    return match fs::read_to_string(filename) {
-        Ok(val) => val,
-        Err(_) => fs::read_to_string("dist/404.md").expect("Error reading 404.md. Is 404.md there?"),
-    };
 }
